261c2b934e916235209f415082107c6ff26bc3fa
Belen Rosas <belenrosas97@gmail.com>
2023-11-08 23:59:11 -0300

    Traduccion y cambios en la aplicacion

---
+++ js/aplicacionCamiones.js
@@ -0,0 +1,45 @@
+import { ruta } from "./variables.js";
+
+function aplicarIngles() {
+    document.cookie = "lang=en"
+    location.reload()
+}
+
+function aplicarEspanol(){
+    document.cookie = "lang=es"
+    location.reload()
+}
+
+$('#idiomaDelSistema').click(function(){
+    if(document.cookie.indexOf("lang=en") !== -1){
+        aplicarEspanol()
+    } else {
+        aplicarIngles()
+    }
+});
+
+$(document).ready(function () {
+    if(document.cookie.indexOf("lang=en") !== -1){
+        $('#idiomaDelSistema').css('background-image', 'url(/img/banderaUK.png)')
+    } else {
+        $('#idiomaDelSistema').css('background-image', 'url(/img/banderaUruguay.png)')
+    }
+    Promise.all([fetch('/' + ruta), fetch('/json/elementos.json')])
+    .then((responses) => Promise.all(responses.map((response) => response.json())))
+    .then((data) => {
+        const idioma = data[0];
+        const arrayDeIdioma = idioma[4]
+        const arrayDeTextos = data[1];
+        const arrayDeTextos2 = arrayDeTextos[4]
+
+        for (let posicion = 0; posicion < Object.keys(arrayDeTextos2).length; posicion++){
+            let texto = document.getElementById(arrayDeTextos2[posicion])
+            if (texto.nodeName == "INPUT"){
+                texto.placeholder = arrayDeIdioma[posicion]
+            } else {
+                texto.textContent = arrayDeIdioma[posicion]
+            }
+        }
+    })
+
+});
